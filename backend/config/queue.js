374741fd47b346fb694f6a27a5784241a65f37df
anand <anandvaibhaw000@gmail.com>
2025-01-10 14:09:54 +0530

    modified to check upon health

--- backend/config/queue.js
+++ backend/config/queue.js
@@ -1,10 +1,17 @@
 const amqp = require('amqplib');
 
 let channel;
+let connection;
 
 const connectQueue = async () => {
-    const connection = await amqp.connect(process.env.RABBITMQ_URL);
-    channel = await connection.createChannel();
+    try {
+        connection = await amqp.connect(process.env.RABBITMQ_URL);
+        channel = await connection.createChannel();
+        console.log("RabbitMQ connected successfully.");
+    } catch (error) {
+        console.error("Failed to connect to RabbitMQ, retrying...", error);
+        setTimeout(connectQueue, 5000);  // Retry in 5 seconds
+    }
 };
 
 const getChannel = () => {
